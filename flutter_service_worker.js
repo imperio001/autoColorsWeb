b12d4ee8bf968ca33705be796ad41b80ab1d3685
imperio001 <i.rodriguezduran@gmail.com>
2024-05-05 20:38:41 -0600

    fix inicial

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"version.json": "e9eb58db72d407be27e9fa052224c304",
 "index.html": "a526995c1bb1757579c9232dbc613897",
 "/": "a526995c1bb1757579c9232dbc613897",
-"main.dart.js": "40a89777b24ca8597764338cd817475b",
+"main.dart.js": "bd92814ca5bade6fa207ef2a374f09ad",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
